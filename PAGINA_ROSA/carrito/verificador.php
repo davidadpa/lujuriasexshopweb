9da86488149c13d8351266ce09a5af5bc2eabe78
Eternalizing <eternaldarkblack@gmail.com>
2023-07-21 21:05:42 -0500

    aa
    aa

--- PAGINA_ROSA/carrito/verificador.php
+++ PAGINA_ROSA/carrito/verificador.php
@@ -6,6 +6,41 @@ include("../template/cabecera_carrito.php");
 
 
 ?>
+<?php
+
+// Obtener el ID de la venta actual
+$sentencia_venta_actual = $conexion->prepare("SELECT MAX(venta_id) AS venta_actual FROM tlb_ventas");
+$sentencia_venta_actual->execute();
+$venta_actual = $sentencia_venta_actual->fetch(PDO::FETCH_ASSOC);
+$venta_actual_id = $venta_actual['venta_actual'];
+
+// Obtener los detalles de la venta actual
+$sentencia_detalles_venta = $conexion->prepare("SELECT * FROM tlb_detalle_ventas WHERE detalle_idventa = :idventa");
+$sentencia_detalles_venta->bindParam(":idventa", $venta_actual_id);
+$sentencia_detalles_venta->execute();
+$detalles_venta = $sentencia_detalles_venta->fetchAll(PDO::FETCH_ASSOC);
+
+// Actualizar el stock de cada producto vendido
+foreach ($detalles_venta as $detalle) {
+    $id_producto = $detalle['detalle_id_producto'];
+    $cantidad_vendida = $detalle['detalle_cantidad'];
+
+    // Obtener el stock actual del producto
+    $sentencia_stock_actual = $conexion->prepare("SELECT producto_cantidad FROM tlb_producto WHERE producto_id = :id_producto");
+    $sentencia_stock_actual->bindParam(":id_producto", $id_producto);
+    $sentencia_stock_actual->execute();
+    $producto = $sentencia_stock_actual->fetch(PDO::FETCH_ASSOC);
+
+    // Calcular el nuevo stock después de la venta
+    $nuevo_stock = $producto['producto_cantidad'] - $cantidad_vendida;
+
+    // Actualizar el stock del producto en la base de datos
+    $sentencia_actualizar_stock = $conexion->prepare("UPDATE tlb_producto SET producto_cantidad = :nuevo_stock WHERE producto_id = :id_producto");
+    $sentencia_actualizar_stock->bindParam(":nuevo_stock", $nuevo_stock);
+    $sentencia_actualizar_stock->bindParam(":id_producto", $id_producto);
+    $sentencia_actualizar_stock->execute();
+}
+?>
 
 
 
